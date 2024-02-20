62ed47da24bb843f1f7cadcf924e73366c647f74
thierry dothee <thierrydothee@protonmail.com>
2024-02-20 10:35:01 +0100

    create page single post with parameter

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Blog;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityNotFoundException;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +16,17 @@ class PostController extends AbstractController
     #[Route('/', name: 'app_post_index', methods: ['GET'])]
     public function index(PostRepository $postRepository, PaginatorInterface $paginatorInterface, Request $request): Response
     {
-        $data = $postRepository->findPublished();
+        try{
+            $data = $postRepository->findPublished();
+        } catch (EntityNotFoundException $ex){
+            echo "Exception Found - " . $ex->getMessage() . "<br/>"; // change the message  with addflash for example !
+        }
         return $this->render('pages/post/index.html.twig', ['posts' => $paginatorInterface->paginate( $data, $request->query->getInt('page', 1),9) ]);
+    }
+
+    #[Route('/article/{slug}',name:'app_post_show',methods:['GET'])]
+    public function show( Post $post): Response
+    {
+        return $this->render('pages/post/show.html.twig',['post'=>$post]);
     }
 }
