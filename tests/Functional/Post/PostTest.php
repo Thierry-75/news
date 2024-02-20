30fe00b6e1e7e66f06a48275586ec530d0ea54d0
thierry dothee <thierrydothee@protonmail.com>
2024-02-20 14:14:16 +0100

    share facebook & twitter on post  + phpunit functional

--- tests/Functional/Post/PostTest.php
+++ tests/Functional/Post/PostTest.php
@@ -2,44 +2,76 @@
 
 namespace App\Tests\Functional\Post;
 
-use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-use function PHPUnit\Framework\assertEquals;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostTest extends WebTestCase
 {
-    public function testBlogPageWorks(): void
+    public function testPostPageWorks(): void
     {
         $client = static::createClient();
-       $client->request(Request::METHOD_GET,'/');
 
-       $this->assertResponseIsSuccessful();
-       $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        /** @var UrlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
 
-      $this->assertSelectorExists('h1');
-       $this->assertSelectorTextContains('h1','Blog créé avec Symfony');
-    }
+        /** @var EntityManagerInterface */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-    public function testPaginationWorks():void
-    {
-        $client = static::createClient();
-        $crawler = $client->request(Request::METHOD_GET, '/');
- 
+        /** @var PostRepository */
+        $postRepository = $entityManager->getRepository(Post::class);
+
+        /** @var Post */
+        $post = $postRepository->findOneBy([]);
+
+        $client->request(
+            Request::METHOD_GET,
+            $urlGeneratorInterface->generate('app_post_show', ['slug' => $post->getSlug()])
+        );
+
+
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
 
-   
+        $this->assertSelectorExists('h1');
+        $this->assertSelectorTextContains('h1', ucfirst(($post->getTitle())));
+    }
 
-        $link = $crawler->selectLink('2')->extract(['href'])[0];
+    public function testReturnToBlog(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+
+        /** @var EntityManagerInterface */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var PostRepository */
+        $postRepository = $entityManager->getRepository(Post::class);
+
+        /** @var Post */
+        $post = $postRepository->findOneBy([]);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGeneratorInterface->generate('app_post_show', ['slug' => $post->getSlug()])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $link =$crawler->selectLink('Retourner au blog')->link()->getUri();
+
         $crawler = $client->request(Request::METHOD_GET, $link);
 
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
-
-        $posts = $crawler->filter('div.card');
-        $this->assertGreaterThanOrEqual(1, count($posts));
+        $this->assertRouteSame('app_post_index');
 
     }
 }
